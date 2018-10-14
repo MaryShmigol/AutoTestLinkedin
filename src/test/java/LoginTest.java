23a63aca115c71fdfb063c51562363db9d82ebf9
Mary Shmigol <maryashmigol@gmail.com>
2018-10-14 21:37:56 +0300

    lessons 4

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -22,11 +23,19 @@ public class LoginTest {
     public void succsessfulLoginTest(){
         WebDriver webDriver = new FirefoxDriver();
         webDriver.get("https://www.linkedin.com/");
-        Assert.assertEquals(webDriver.getCurrentUrl(),"https://www.linkedin.com/", "home page url is wrong");
+        Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/",
+                "Home page URL is wrong.");
 
 
 
-
+            WebElement loginField = ((FirefoxDriver) webDriver).findElementById("login-email");
+            loginField.sendKeys("klymenkosergey87@gmail.com");
+            WebElement passwordFiels = ((FirefoxDriver) webDriver).findElementById("login-password");
+            passwordFiels.sendKeys("vera228606");
+            WebElement loginButton = ((FirefoxDriver) webDriver).findElementById("login-submit");
+            loginButton.click();
+            Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/feed/","URL is wrong" );
         webDriver.quit();
+
     }
 }
